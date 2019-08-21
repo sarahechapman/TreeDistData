05e5b0bdda31f5f78ed8809a164cc464a6387179
Martin Smith <martins@gmail.com>
2019-08-21 15:09:56 +0100

    randomTreeDistances

--- data-raw/randomTreeDistances.R
+++ data-raw/randomTreeDistances.R
@@ -1,17 +1,23 @@
 library('TreeDistData')
+library('usethis')
 suppressWarnings(RNGversion("3.5.0")) # Stopgap until we can require R 3.6.0
 repls <- 1000
 
-if (!exists('randomTreeDistances')) {
+# Look for existing data object
+use_directory('data')
+paths <- fs::path('data', 'randomTreeDistances', ext='rda')
+if (file.exists(proj_path(paths))) {
+  load(proj_path(paths))
+} else {
   randomTreeDistances <- array(NA, #dim=c(197, 10, 13),
                                dim = c(10, 13, 197),
                                dimnames =
-    list(
-      c('vpi', 'vmsi', 'vci', 'qd', 'nts', 'msd', 'rf',
-                            'path', 'spr', 'sprLB'),
-      c('min', '1%', '5%', '10%', '25%', '50%', '75%',
-                            '90%', '95%', '99%', 'max', 'mean', 'sd'),
-      4:200))
+                                 list(
+                                   c('vpi', 'vmsi', 'vci', 'qd', 'nts', 'msd', 'rf',
+                                     'path', 'spr', 'sprLB'),
+                                   c('min', '1%', '5%', '10%', '25%', '50%', '75%',
+                                     '90%', '95%', '99%', 'max', 'mean', 'sd'),
+                                   4:200))
   usethis::use_data(randomTreeDistances, compress='gzip', overwrite=TRUE)
 }
 
@@ -34,16 +40,20 @@ RandomDistances <- function (nLeaves, repls) {
                       },
                       double(10L))
   t(rbind(apply(distances, 1L, summary),
-        apply(distances, 1L, quantile, probs=c(0.01, 0.05, 0.1, 0.9, 0.95, 0.99)),
-        sd = apply(distances, 1L, sd)
+          apply(distances, 1L, quantile, probs=c(0.01, 0.05, 0.1, 0.9, 0.95, 0.99)),
+          sd = apply(distances, 1L, sd)
   )[c(1, 7:9, 2, 3, 5, 10:12, 6, 4, 13), ])
 }
 
 # Build steadily so that partial dataset is available,
 # and so that progress is not lost if script interrupted.
 while (any(empty <- is.na(randomTreeDistances[1, 1, ]))) {
+  cat(sum(empty), 'to go...\n')
   doNext <- sample(names(empty), 1)
   cat('\n', doNext, 'Leaves ')
-  randomTreeDistances[, , doNext] <- RandomDistances(as.integer(doNext), repls)
+  dists <- RandomDistances(as.integer(doNext), repls)
+  load(proj_path(paths))
+  cat(ifelse(empty, '-', 'X'))
+  randomTreeDistances[, , doNext] <- dists
   usethis::use_data(randomTreeDistances, compress='gzip', overwrite=TRUE)
 }
