78ccba2711bb5da050b44e60019c1afbd530a3e7
Martin Smith <martins@gmail.com>
2019-11-05 11:29:53 +0000

    expect_equivalent

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -4,5 +4,5 @@ test_that('Pairwise distances calculated correctly', {
   trees <- lapply(rep(16, 6), ape::rtree, br=NULL)
   dists <- PairwiseDistances(trees, phangorn::RF.dist)
   expect_equal(dists, t(dists))
-  expect_equal(as.integer(TreeDist::RobinsonFoulds(trees)), as.integer(dists))
+  expect_equivalent(TreeDist::RobinsonFoulds(trees), dists)
 })
