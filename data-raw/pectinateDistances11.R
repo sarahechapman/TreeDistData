523cb280d81201e56851bdb84d4c30609d60b406
Martin Smith <martins@gmail.com>
2019-12-12 14:16:35 +0000

    Tree distances v2.0

--- data-raw/pectinateDistances11.R
+++ data-raw/pectinateDistances11.R
@@ -1,34 +1,60 @@
+library('TreeTools')
 library('TreeDist')
 suppressWarnings(RNGversion("3.5.0")) # Stopgap until we can require R 3.6.0
-set.seed(0)
 
-pectinateTree <- structure(list(edge = structure(c(12L, 13L, 14L, 15L, 16L, 17L,
-                                                    17L, 16L, 15L, 14L, 13L, 12L, 18L, 18L, 19L, 19L, 20L, 20L, 21L,
-                                                    21L, 13L, 14L, 15L, 16L, 17L, 1L, 2L, 3L, 4L, 5L, 6L, 18L, 7L,
-                                                    19L, 8L, 20L, 9L, 21L, 10L, 11L), .Dim = c(20L, 2L)), Nnode = 10L,
-                                 tip.label = c("1", "2", "3", "4", "5", "6", "7", "8", "9",
-                                               "10", "11")), class = "phylo", order = "cladewise")
+nTip <- 11L
+tipLabel <- seq_len(nTip)
+pectinateTree <- PectinateTree(tipLabel)
 
 repls <-  100000L
+set.seed(0)
+randomTreeIds <- unique(floor(runif(repls * 2) * NUnrooted(nTip)))[seq_len(repls)]
+randomTrees <- as.phylo(randomTreeIds, nTip, tipLabel)
+randomTrees <- structure(lapply(randomTrees, Postorder), class = 'multiPhylo')
+message("Generated ", repls, " comparison trees.")
 
-tipLabel <- pectinateTree$tip.label
-randomTrees <- lapply(seq_len(repls), function (i) ape::rtree(11L, br=NULL, tip.label=tipLabel))
-cat("Generated comparison trees.")
+message('Calculating VoI... ')
+vpi <- VariationOfPhylogeneticInfo(pectinateTree, randomTrees, normalize=TRUE)
+vmsi <- VariationOfMatchingSplitInfo(pectinateTree, randomTrees, normalize=TRUE)
+vci <- VariationOfClusteringInfo(pectinateTree, randomTrees, normalize=TRUE)
+message('QD... ')
+qd <- Quartet::QuartetDivergence(Quartet::QuartetStatus(randomTrees, cf=pectinateTree), similarity = FALSE)
+message('NTS... ')
+nts <- 1 - NyeTreeSimilarity(pectinateTree, randomTrees, normalize=TRUE)
+message('MSD... ')
+msd <- MatchingSplitDistance(pectinateTree, randomTrees)
+message('MAST... ')
+mast <- MASTSize(pectinateTree, randomTrees)
+message('NNI... ')
+nni <- NNIDist(pectinateTree, randomTrees)
+rownames(nni) <- c('nni_l', 'nni_t', 'nni_u')
+message('SPR... ')
+spr <- phangorn::SPR.dist(pectinateTree, randomTrees)
+message('TBR... ')
+tbr <- TBRDist::TBRDist(pectinateTree, randomTrees)
+message('MAFi... ')
+mafi <- TBRDist::MAFInfo(pectinateTree, randomTrees)
+message('RF... ')
+rf <- RobinsonFoulds(pectinateTree, randomTrees)
+message('Path... ')
+path <- phangorn::path.dist(pectinateTree, randomTrees)
 
-pectinateDistances11 <- rbind(
-  VariationOfPhylogeneticInfo(pectinateTree, randomTrees, normalize=TRUE),
-  VariationOfMatchingSplitInfo(pectinateTree, randomTrees, normalize=TRUE),
-  VariationOfClusteringInfo(pectinateTree, randomTrees, normalize=TRUE),
-  Quartet::QuartetDivergence(Quartet::QuartetStatus(randomTrees, cf=pectinateTree), similarity = FALSE),
-  1 - NyeTreeSimilarity(pectinateTree, randomTrees, normalize=TRUE),
-  MatchingSplitDistance(pectinateTree, randomTrees),
-  vapply(randomTrees, phangorn::treedist, tree2=pectinateTree, double(2)),
-  vapply(randomTrees, phangorn::SPR.dist, tree2=pectinateTree, double(1))
-)
-cat("Calculated and bound.")
+pectinateDistances11 <- rbind(vpi = vpi, vmsi = vmsi, vci = vci, qd = qd,
+                              nts = nts, mst = msd,
+                              mast = mast, masti = LnUnrooted(mast) / log(2),
+                              nni, spr = spr,
+                              tbr_l = tbr$tbr_min, tbr_u = tbr$tbr_max,
+                              mafi = mafi, rf = rf, path = path)
 
-rownames(pectinateDistances11) <- names(c(vpi = 0, vmsi = 0, vci = 0, qd = 0, nts = 0, msd = 0, rf = 0, path = 0,
-     spr = 0))
+
+message("Calculated and bound.")
+
+normalizers <- c(vpi = 1, vmsi = 1, vci = 1, qd = 1, nts = 1, msd = 1,
+                 mast=nTip, masti=LnUnrooted.int(nTip) / log(2),
+                 nni_l=18, nni_t = 18, nni_u= 18,
+                 spr = 18, tbr_l =18, tbr_u = 18,
+                 mafi = 1,
+                 rf = 1, path = 1)
 
 usethis::use_data(pectinateDistances11, compress='xz', overwrite=TRUE)
-cat("Used data. Complete.")
+message("Used data. Complete.")
