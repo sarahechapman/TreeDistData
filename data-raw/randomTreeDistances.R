2fb27517eed1209de8a01831457a1736b872340b
Martin Smith <martins@gmail.com>
2020-01-20 13:36:26 +0000

    Ignore nni_t: NAs can't be handled

--- data-raw/randomTreeDistances.R
+++ data-raw/randomTreeDistances.R
@@ -2,6 +2,12 @@ library('TreeDistData')
 library('usethis')
 suppressWarnings(RNGversion("3.5.0")) # Stopgap until we can require R 3.6.0
 repls <- 1000
+allDistMethods <- c('vpi', 'vmsi', 'vci', 'qd', 'nts',
+                    'ja2', 'ja4', 'jna2', 'jna4',
+                    'msd', 'mast', 'masti',
+                    'nni_l', 'nni_t', 'nni_u', 'spr', 'tbr_l', 'tbr_u',
+                    'rf', 'path')
+ourMethods <- allDistMethods[!allDistMethods %in% 'nni_t']
 
 # Look for existing data object
 use_directory('data')
@@ -9,15 +15,11 @@ paths <- fs::path('data', 'randomTreeDistances', ext='rda')
 if (file.exists(proj_path(paths))) {
   load(proj_path(paths))
 } else {
-  methodAc <- c('vpi', 'vmsi', 'vci', 'qd', 'nts', 'ja2', 'ja4', 'jna2', 'jna4',
-                'msd', 'mast', 'masti',
-                'nni_l', 'nni_t', 'nni_u', 'spr', 'tbr_l', 'tbr_u',
-                'rf', 'path')
-  randomTreeDistances <- array(NA, #dim=c(197, 10, 13),
-                               dim = c(length(methodAc), 13, 197),
+  randomTreeDistances <- array(NA,
+                               dim = c(length(ourMethods), 13, 197),
                                dimnames =
                                  list(
-                                   methodAc,
+                                   ourMethods,
                                    c('min', '1%', '5%', '10%', '25%', '50%', '75%',
                                      '90%', '95%', '99%', 'max', 'mean', 'sd'),
                                    4:200))
@@ -28,15 +30,18 @@ RandomDistances <- function (nLeaves, repls) {
   RandomTree <- function (nTip) ape::rtree(nTip, br = NULL)
   distances <- vapply(seq_len(repls),
                       function (XX) {
-                        if (XX %% 72 == 0) cat("\n           ")
+                        cat(".")
+                        if (XX %% 72 == 0) cat(' ', XX, "\n           ")
                         tr1 <- RandomTree(nLeaves)
                         tr2 <- RandomTree(nLeaves)
                         TreeDistData:::AllDists(tr1, tr2)
                       },
-                      double(20L))
+                      double(length(allDistMethods)))
+  distances <- distances[allDistMethods %in% ourMethods, ]
   t(rbind(apply(distances, 1L, summary),
-          apply(distances, 1L, quantile, probs=c(0.01, 0.05, 0.1, 0.9, 0.95, 0.99)),
-          sd = apply(distances, 1L, sd)
+          apply(distances, 1L, quantile,
+                probs = c(0.01, 0.05, 0.1, 0.9, 0.95, 0.99)),
+          sd = apply(distances, 1L, sd, na.rm = TRUE)
   )[c(1, 7:9, 2, 3, 5, 10:12, 6, 4, 13), ])
 }
 
