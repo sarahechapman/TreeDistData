3bd25f3530a9c94494e237a71d1f7c122683eccc
Martin Smith <martins@gmail.com>
2020-01-29 16:41:17 +0000

    Remove stale tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,13 +1,10 @@
 context("Loading data")
 
 test_that("Data dimensions are correct", {
-  nMetrics <- 9L
+  nMetrics <- 19L
 
   data("sevenTipDistances", package='TreeDistData')
   expect_equal(nMetrics + 1L, length(sevenTipDistances))
-  expect_equal(945L, length(sevenTipDistances$shapes))
-  vapply(sevenTipDistances[seq_len(nMetrics)], function (item)
-    expect_equal(c(945L, 945L), dim(item)), integer(2))
 
   data("distanceDistribution25", package='TreeDistData')
   expect_equal(c(nMetrics, 10000L), dim(distanceDistribution25))
