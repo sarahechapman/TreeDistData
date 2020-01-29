2f41093f3abca9e1440f7d8ef896785369508a57
Martin Smith <martins@gmail.com>
2020-01-29 16:50:54 +0000

    Cheeky deprecation: to remove!

--- R/utilities.R
+++ R/utilities.R
@@ -209,5 +209,6 @@ CompareAllTrees <- function (trees, exact = FALSE, slow = TRUE,
 #'
 #' @export
 TreeDistCol <- function (method, opacity = 'FF') {
+  if (method %in% c('vpi', 'vci', 'vmsi')) .Deprecated('New abbreviations')
   paste0(TreeDistData::tdCol[method], opacity)
 }
