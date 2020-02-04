d3a0bb1286e93ebdf373c4ad45ddf34758336acf
Martin Smith <martins@gmail.com>
2020-02-04 10:11:28 +0000

    Update distanceDistribution calculation

--- data-raw/distanceDistribution50.R
+++ data-raw/distanceDistribution50.R
@@ -13,7 +13,7 @@ distanceDistribution50 <- vapply(randomTreePairs50, function (treePair) {
 }, c(dpi = 0, msid = 0, cid = 0, qd = 0, nts = 0, ja2 = 0, ja4 = 0,
      jna2 = 0, jna4 = 0, msd = 0, mast = 0, masti = 0, nni_l = 0,
      nni_t = 0, nni_u = 0, spr = 0, tbr_l = 0, tbr_u = 0, rf = 0,
-     path = 0)
+     rfi = 0, path = 0)
 )
 
 usethis::use_data(randomTreePairs50, compress='bzip2', overwrite=TRUE)
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,25 +1,24 @@
 context("Loading data")
 
 test_that("Data dimensions are correct", {
-  nMetrics <- 20L
+  data('tdMethods', package = 'TreeDistData')
+  nMetrics <- length(tdMethods)
+
+  LengthWithout <- function(...) nMetrics - length(list(...))
 
   data("sevenTipDistances", package='TreeDistData')
-  expect_equal(nMetrics, length(sevenTipDistances))
+  expect_equal(LengthWithout('mafi'), length(sevenTipDistances))
   expect_equal(c(945L, 945L), dim(sevenTipDistances[[1]]))
 
   data("distanceDistribution25", package='TreeDistData')
-  expect_equal(c(nMetrics - 1L, 10000L), dim(distanceDistribution25))
+  expect_equal(c(LengthWithout('mafi', 'nni_t'), 10000L),
+               dim(distanceDistribution25))
+  expect_equal(dim(distanceDistribution50), dim(distanceDistribution25))
 
   #TODO update other datasets for all 20 metrics?
-  allDistMethods <- c('dpi', 'msid', 'cid', 'qd', 'nts', 'ja2', 'ja4', 'jna2',
-                      'jna4', 'msd', 'mast', 'masti', 'nni_l', 'nni_t', 'nni_u',
-                      'spr', 'tbr_l', 'tbr_u', 'rf', 'path')
-  metrics <- allDistMethods[!allDistMethods %in% 'nni_t']
-  nMetrics <- length(metrics)
   data("randomTreeDistances", package='TreeDistData')
   nLeafMeasurements <- 197L
-  expect_equal(c(nMetrics, 13L, nLeafMeasurements),
+  expect_equal(c(LengthWithout('mafi', 'nni_t'), 13L, nLeafMeasurements),
                dim(randomTreeDistances))
-  expect_equal(sort(metrics), sort(dimnames(randomTreeDistances)[[1]]))
 
 })
