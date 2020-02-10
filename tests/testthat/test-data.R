ee06ee241e96d61b6bb86841d4e4e6db0efe6c60
Martin Smith <martins@gmail.com>
2020-02-10 13:37:23 +0000

    Messages

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,15 +1,19 @@
 context("Loading data")
 
 test_that("Data dimensions are correct", {
+  message('D-4')
   data('tdMethods', package = 'TreeDistData')
   nMetrics <- length(tdMethods)
 
+  message('D-8')
   LengthWithout <- function(...) nMetrics - length(list(...))
 
+  message('D-7td')
   data("sevenTipDistances", package='TreeDistData')
   expect_equal(LengthWithout('mafi'), length(sevenTipDistances))
   expect_equal(c(945L, 945L), dim(sevenTipDistances[[1]]))
 
+  message('D-dd25')
   data("distanceDistribution25", package='TreeDistData')
   expect_equal(c(LengthWithout('mafi'), 10000L),
                dim(distanceDistribution25))
@@ -17,13 +21,16 @@ test_that("Data dimensions are correct", {
 
   noMafiNnit <- tdMethods[!tdMethods %in% c('mafi', 'nni_t')]
 
+  message('D-rtd')
   data("randomTreeDistances", package='TreeDistData')
   nLeafMeasurements <- 197L
   expect_true(all(noMafiNnit %in% dimnames(randomTreeDistances)[[1]]))
   expect_equal(c(LengthWithout('mafi', 'nni_t'), 13L, nLeafMeasurements),
                dim(randomTreeDistances))
 
+  message('D-bull')
   expect_true(all(noMafiNnit %in% names(bullseyeDistances[[1]])))
   expect_true(all(noMafiNnit %in% dimnames(bullseyeMorphScores[[1]])[[2]]))
   #TODO update other datasets for all 20 metrics?
+  message('D-one')
 })
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -4,28 +4,35 @@ test_that('Pairwise distances calculated correctly', {
   nTrees <- 6L
   nTip <- 16L
 
+  message('U-7')
   set.seed(0)
   trees <- lapply(rep(nTip, nTrees), ape::rtree, br=NULL)
   trees[[1]] <- TreeTools::BalancedTree(nTip)
   trees[[nTrees - 1L]] <- TreeTools::PectinateTree(nTip)
   class(trees) <- 'multiPhylo'
 
+  message('U-14')
   dists <- PairwiseDistances(trees, phangorn::RF.dist)
   expect_equivalent(phangorn::RF.dist(trees), dists)
 
+  message('U-18')
   lapply(CompareAllTrees(trees), function (dist) {
     expect_equal(c(nTrees, nTrees), dim(as.matrix(dist)))
   })
 
 
+  message('U-24')
   sprWalk <- vector('list', nTrees)
   sprWalk[[1]] <- lastTree <- TreeTools::PectinateTree(nTip)
 
+  message('U-28')
   for (i in seq.int(2, nTrees)) {
     sprWalk[[i]] <- lastTree <- TreeSearch::SPR(lastTree)
   }
 
+  message('U-33')
   lapply(CompareAllTrees(sprWalk), function(dist) {
     expect_equal(c(nTrees, nTrees), dim(as.matrix(dist)))
   })
+  message('U-##')
 })
