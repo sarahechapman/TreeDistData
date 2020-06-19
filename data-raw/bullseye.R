683ab62a73be32303999eb21c8a18638e2fdc375
Martin Smith <martins@gmail.com>
2020-06-19 14:35:40 +0100

    Spaces

--- data-raw/bullseye.R
+++ data-raw/bullseye.R
@@ -4,7 +4,7 @@ library('TreeDist')
 library('TreeSearch')
 
 
-data("bullseyeTrees", package='TreeDistData') # Generated in bullseyeTrees.R
+data("bullseyeTrees", package = 'TreeDistData') # Generated in bullseyeTrees.R
 data('bullseyeSeqs', package = 'TreeDistData') # Generated in bullseyeSeqs.R
 tipsNames <- names(bullseyeTrees)
 nTrees <- length(bullseyeTrees[[1]])
@@ -25,8 +25,9 @@ for (trees in tipsNames[seq_len(useML)]) {
 
     lapply(subsamples, function (n) {
       fitStart <- pml(unroot(tr), MatrixToPhyDat(sq[, seq_len(n)]))
-      optim.pml(fitStart, model="K80", optNni=TRUE, rearrangement="stochastic",
-                control=pml.control(epsilon=1e-06, trace = 0))$tree
+      optim.pml(fitStart, model = "K80", optNni = TRUE,
+                rearrangement = "stochastic",
+                control = pml.control(epsilon = 1e-06, trace = 0))$tree
     })
   })
 }
@@ -46,7 +47,7 @@ for (trees in tipsNames[-seq_len(useML)]) {
     })
   })
 }
-usethis::use_data(bullseyeInferred, compress='xz', overwrite=TRUE)
+usethis::use_data(bullseyeInferred, compress = 'xz', overwrite = TRUE)
 
 bullseyeScores <- vector('list', length(nTips))
 names(bullseyeScores) <- tipsNames
@@ -56,25 +57,26 @@ for (trees in tipsNames) {
   theseScores <- vapply(seq_along(inferred), function (i) {
     trueTree <- theseTrees[[i]]
     rootTip <- trueTree$tip.label[1]
-    tr <- root(trueTree, rootTip, resolve.root=TRUE)
+    tr <- root(trueTree, rootTip, resolve.root = TRUE)
     tr$edge.length  <- NULL
-    trs <- lapply(inferred[[i]], root, rootTip, resolve.root=TRUE)
+    trs <- lapply(inferred[[i]], root, rootTip, resolve.root = TRUE)
 
     normInfo <- SplitwiseInfo(tr)
     cbind(
-      vpi = VariationOfPhylogeneticInfo(tr, trs, normalize=normInfo),
-      vmsi = VariationOfMatchingSplitInfo(tr, trs, normalize=normInfo),
-      vci = VariationOfClusteringInfo(tr, trs, normalize=normInfo),
-      qd = Quartet::QuartetDivergence(Quartet::QuartetStatus(trs, cf=tr), similarity = FALSE),
-      nts = 1 - NyeSimilarity(tr, trs, normalize=TRUE),
+      vpi = VariationOfPhylogeneticInfo(tr, trs, normalize = normInfo),
+      vmsi = VariationOfMatchingSplitInfo(tr, trs, normalize = normInfo),
+      vci = VariationOfClusteringInfo(tr, trs, normalize = normInfo),
+      qd = Quartet::QuartetDivergence(Quartet::QuartetStatus(trs, cf = tr),
+                                      similarity = FALSE),
+      nts = 1 - NyeSimilarity(tr, trs, normalize = TRUE),
       msd = MatchingSplitDistance(tr, trs),
-      t(vapply(trs, phangorn::treedist, tree2=tr, double(2))),
-      spr = vapply(trs, phangorn::SPR.dist, tree2=tr, double(1))
+      t(vapply(trs, phangorn::treedist, tree2 = tr, double(2))),
+      spr = vapply(trs, phangorn::SPR.dist, tree2 = tr, double(1))
     )
-  }, matrix(0, nrow = 10L, ncol=9L, dimnames=list(
+  }, matrix(0, nrow = 10L, ncol = 9L, dimnames = list(
     10:1 * 200,
     c('vpi', 'vmsi', 'vci', 'qd', 'nts', 'msd', 'rf', 'path', 'spr')
   )))
   bullseyeScores[[trees]] <- theseScores
 }
-usethis::use_data(bullseyeScores, compress='xz', overwrite=TRUE)
+usethis::use_data(bullseyeScores, compress = 'xz', overwrite = TRUE)
